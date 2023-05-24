a27d360288481ecbcf8007a83f92a6b41d65a67b
konst <konst@Konstantin>
2023-05-24 21:25:12 +0200

    Update 0.2

--- com.konsteyyy.git.uni.dea/src/com/konsteyyy/git/uni/dea/ListTest.java
+++ com.konsteyyy.git.uni.dea/src/com/konsteyyy/git/uni/dea/ListTest.java
@@ -4,6 +4,16 @@ public class ListTest {
 
 	public static void main(String[] args) {
 		SList<Integer> test = new SList<Integer>();
+		test.append(10);
+		test.append(12);
+		test.append(35);
+		
+		System.out.println(test.toString());
+		
+		test.delete(2);
+		System.out.println(test.toString());
+		
+		
 	}
 
 }
--- com.konsteyyy.git.uni.dea/src/com/konsteyyy/git/uni/dea/SList.java
+++ com.konsteyyy.git.uni.dea/src/com/konsteyyy/git/uni/dea/SList.java
@@ -16,44 +16,78 @@ public class SList<T> implements IList<T> {
 	@Override
 	public boolean empty() {
 		//if the list length is zero, it is empty
-		return (size() == 0);
+		return (head == null);
 	}
 
 	@Override
 	public void clear() {
-		ListElement<T> current = head;
-		while(current.getNext() != null) {
-			current = null;
-			current = current.getNext();
-		}
-		current = null;
+		//Garbage collector frees all other elements
+		head = null;
 	}
 
 	@Override
 	public void append(T el) {
-		ListElement<T> current = head;
-		while(current.getNext() != null) {
-			current = current.getNext();
+		ListElement<T> newElement = new ListElement<T>(el);
+		if(empty())
+			head = newElement;
+		else {
+			ListElement<T> current = head;
+			while(current.getNext() != null) {
+				current = current.getNext();
+			}
+			current.setNext(newElement); 
 		}
-		current.setNext(new ListElement<T>(el));
 	}
 
 	@Override
-	public void insert(Object el, int pos) {
-		// TODO Auto-generated method stub
-		
+	public void insert(T el, int pos) {
+		ListElement<T> newElement = new ListElement<T>(el);
+		if(pos == 0) {
+			//new element is the new head
+			newElement.setNext(head);
+			head = newElement;
+		}
+		else {
+			ListElement<T> current = head;
+			//iterate through list until iterator is an element after list or right position reached
+			while(current != null && pos >=0) {
+				current = current.getNext();
+				pos--;
+			}
+			if(current != null) {
+				newElement.setNext(current.getNext());
+				current.setNext(newElement); 
+			} else {
+				throw new IndexOutOfBoundsException("Ungültige Position: " + pos);
+			}
+		}
 	}
 
 	@Override
 	public void delete(int pos) {
-		// TODO Auto-generated method stub
+		if(pos == 0) {
+			//new element is the new head
+			head = head.getNext();
+		}
+		else {
+			ListElement<T> current = head;
+			while(current.getNext() != null && pos >=0) {
+				current = current.getNext();
+				pos--;
+			}
+			if(current.getNext() != null) {
+				//garbage collector automatically frees old element
+				current.setNext(current.getNext().getNext());
+			} else {
+				throw new IndexOutOfBoundsException("Ungültige Position: " + pos);
+			}
+		}
 		
 	}
 
 	@Override
 	public void deleteLast() {
-		
-		
+		delete(size()-1);
 	}
 
 	@Override
@@ -87,7 +121,15 @@ public class SList<T> implements IList<T> {
 	
 	@Override
 	public String toString() {
-		return "";
+		String string = "[";
+		ListElement<T> current = head;
+		while(current != null) {
+			string+=current.getData();
+			current = current.getNext();
+			string += current != null? ",":"";
+		}
+		string += "]";
+		return string;
 	}
 	
 }
