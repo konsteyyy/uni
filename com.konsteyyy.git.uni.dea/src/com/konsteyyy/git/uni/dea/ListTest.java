8f8bd84379628f4d275420ead1965e1fb36c7cb8
konsteyyy <konstantin@konstischaller.de>
2023-05-25 13:07:19 +0200

    Update 0.3 main Methode finished

--- com.konsteyyy.git.uni.dea/src/com/konsteyyy/git/uni/dea/ListTest.java
+++ com.konsteyyy.git.uni.dea/src/com/konsteyyy/git/uni/dea/ListTest.java
@@ -5,23 +5,67 @@ import java.util.Scanner;
 public class ListTest {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		
 		IList<Integer> liste;
-		
-		System.out.println("slist/dlist ?: ");
-		String listType = sc.next();
-		if(listType == "slist")
+		String listType = args[0];
+		String test = "slist";
+		if(listType == test) {
 			liste = new SList<Integer>();
-		else if(listType == "dlist")
+			doActions(liste);
+		}
+		else if(listType == "dlist") {
 			liste = new DList<Integer>();
+			doActions(liste);
+		}
 		
+		
+	}
+
+	public static void doActions(IList<Integer> liste) {
+		Scanner sc = new Scanner(System.in);
 		char action;
 		do {
 			System.out.println("Welche aktion soll ausgeführt werden? (a/i/d/g/r/p/c/s/e):");
 			action = sc.next().charAt(0);
-			//append here
-		} while(action != 'c');
+			switch(action) {
+			case 'a':
+				System.out.println("Was soll der Liste hinzugefügt werden?");
+				int aelement = sc.nextInt();
+				liste.append(aelement);
+				break;
+			case 'i':
+				System.out.println("Was soll der Liste hinzugefügt werden?");
+				int ielement = sc.nextInt();
+				System.out.println("An welcher Position soll das hinzugefügt werden?");
+				int iposition= sc.nextInt();
+				liste.insert(ielement, iposition);
+				break;
+			case 'd':
+				System.out.println("An welcher Stelle soll etwas gelöscht werden?");
+				int dposition = sc.nextInt();
+				liste.delete(dposition);
+				break;
+			case 'g':
+				System.out.println("Welche Position wollen Sie ausgeben?");
+				int gposition = sc.nextInt();
+				System.out.println(liste.get(gposition));
+				break;
+			case 'r':
+				liste.reverse();
+				break;
+			case 'p':
+				System.out.println(liste.toString());
+				break;
+			case 'c':
+				liste.clear();
+				break;
+			case 's':
+				System.out.println(liste.size());
+				break;
+			default:
+				break;
+			}
+		} while(action != 'e');
 	}
-
+	
 }
